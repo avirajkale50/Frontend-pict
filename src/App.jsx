2bfdd316220a90e47cf4c1304fffd64625282022
HarshadSanjayKarale <harshadkarale0@gmail.com>
2025-01-13 23:03:43 +0530

    information page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-
+import Information from './pages/Information'
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/information" element={<Information />} />
+      </Routes>
+    </Router>
   );
 }
 
